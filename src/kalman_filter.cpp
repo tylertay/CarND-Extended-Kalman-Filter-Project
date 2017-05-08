55581378084bb79983d9b6c6d814df2d9dee0397
YongKiat <ytay024@e.ntu.edu.sg>
2017-05-08 12:36:55 +0800

    Intiial commit

---
+++ src/kalman_filter.cpp
@@ -0,0 +1,100 @@
+#include "kalman_filter.h"
+
+using Eigen::MatrixXd;
+using Eigen::VectorXd;
+
+KalmanFilter::KalmanFilter() {}
+
+KalmanFilter::~KalmanFilter() {}
+
+MatrixXd getH(const VectorXd& x_state);
+
+void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
+}
+
+void KalmanFilter::Predict() {
+  /**
+  TODO:
+    * predict the state
+  */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
+}
+
+void KalmanFilter::Update(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Kalman Filter equations
+  */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+void KalmanFilter::UpdateEKF(const VectorXd &z) {
+  /**
+  TODO:
+    * update the state by using Extended Kalman Filter equations
+  */
+  VectorXd z_pred = getH(x_);
+  VectorXd y = z - z_pred;
+  if (y(1)>M_PI){
+    y(1) -= 2*M_PI;
+  }
+  if (y(1)<-M_PI){
+    y(1) += 2*M_PI;
+  }
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
+
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
+}
+
+// return h(x')
+MatrixXd getH(const VectorXd& x_state) {
+  VectorXd h = VectorXd(3);
+
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  //check division by zero
+  float rho = sqrt(px*px + py*py);
+  float rho_dot;
+  if (rho < .0001) {
+    rho_dot = (px*vx + py*vy) / 0.0001;
+  } else {
+    rho_dot = (px*vx + py*vy) / rho;
+  }
+  float phi = atan2(py, px);
+
+  h << rho, phi, rho_dot;
+
+  return h;
+}
